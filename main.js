86c07ca5a62d64b47baa51cbf2d66ca0c7cd3d3b
pipit-f <pipitfitriani667@gmail.com>
2020-10-02 23:07:56 +0700

    Update FILE js,css dan HTML

--- main.js
+++ main.js
@@ -17,3 +17,20 @@ const closeModal = ()=>{
     let modal = document.getElementById('modalMenu')
     modal.style.display = 'none'
 }
+
+//Kita ambil dulu element yang diinginkan
+//pindahkan ke element div
+let showcase = document.getElementById('wrapper_showcase')
+
+//kita inject elementnya dengan syntax HTML
+//showcase.innerHTML +='
+// <img src="https://picsum.photos/seed/13/300"alt="gambar no 13"/>
+//`
+
+for(var i = 0 ; i < 24 ; i++){
+showcase.innerHTML += `
+    <img src='https://picsum.photos/seed/${24 + i}/150' alt='gambar ke ${1}'/>
+`
+
+}
+
